44600b81c40f01718f9ebfa7aae15277e95c2c10
votantaiH <22130244@st.hcmuaf.edu.vn>
2024-08-21 22:07:24 +0700

    a

--- src/main/java/com/hiusahald/shop_online/exception/ExceptionResponse.java
+++ src/main/java/com/hiusahald/shop_online/exception/ExceptionResponse.java
@@ -1,7 +1,18 @@
 package com.hiusahald.shop_online.exception;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Builder;
+import org.springframework.http.HttpStatus;
+
+import java.util.Map;
+import java.util.Set;
+
+@Builder
 public record ExceptionResponse(
-
+        HttpStatus businessCode,
+        String businessMessage,
+        Set<String> validationErrors,
+        String messageError,
+        Map<String, String> messageErrors
 ) {
-
 }
--- src/main/java/com/hiusahald/shop_online/exception/GlobalExceptionHandler.java
+++ src/main/java/com/hiusahald/shop_online/exception/GlobalExceptionHandler.java
@@ -1,21 +1,91 @@
 package com.hiusahald.shop_online.exception;
 
+import com.hiusahald.shop_online.exception.exception.ClientRequestException;
+import com.hiusahald.shop_online.exception.exception.ResentEmailException;
+import com.hiusahald.shop_online.exception.exception.UserExistedException;
 import jakarta.mail.MessagingException;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import static com.hiusahald.shop_online.exception.BusinessError.*;
+
+import java.util.stream.Collectors;
+
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    @ExceptionHandler(ClientRequestException.class)
+    public ResponseEntity<ExceptionResponse> handler(ClientRequestException e) {
+        return ResponseEntity.badRequest().body(
+                ExceptionResponse.builder()
+                        .messageError(e.getMessage())
+                        .build()
+        );
+    }
+
+    @ExceptionHandler(ResentEmailException.class)
+    public ResponseEntity<ExceptionResponse> handler(ResentEmailException e) {
+        return ResponseEntity
+                .status(RESEND_EMAIL.getCode())
+                .body(
+                        ExceptionResponse.builder()
+                                .businessMessage(RESEND_EMAIL.getMessage())
+                                .messageError(e.getMessage())
+                                .build()
+                );
+    }
+
+    @ExceptionHandler(UserExistedException.class)
+    public ResponseEntity<ExceptionResponse> handler(UserExistedException e) {
+        return ResponseEntity
+                .status(USER_EXISTED.getCode())
+                .body(
+                        ExceptionResponse.builder()
+                                .businessMessage(USER_EXISTED.getMessage())
+                                .messageError(e.getMessage())
+                                .build()
+                );
+    }
+
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ExceptionResponse> handler(MethodArgumentNotValidException e) {
-        return null;
+        return ResponseEntity.badRequest().body(
+                ExceptionResponse.builder()
+                        .validationErrors(
+                                e.getBindingResult()
+                                        .getAllErrors()
+                                        .stream()
+                                        .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                                        .collect(Collectors.toSet())
+                        ).build()
+        );
     }
 
     @ExceptionHandler(MessagingException.class)
     public ResponseEntity<ExceptionResponse> handler(MessagingException e) {
-        return null;
+        return ResponseEntity.internalServerError()
+                .body(
+                        ExceptionResponse.builder()
+                                .businessCode(ERROR_SERVER.getCode())
+                                .messageError(e.getMessage())
+                                .build()
+                );
     }
+
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ExceptionResponse> handler(Exception e) {
+        return ResponseEntity.internalServerError()
+                .body(
+                        ExceptionResponse.builder()
+                                .businessCode(ERROR_SERVER.getCode())
+                                .businessMessage(ERROR_SERVER.getMessage())
+                                .messageError(e.getMessage())
+                                .build()
+                );
+    }
+
 }
